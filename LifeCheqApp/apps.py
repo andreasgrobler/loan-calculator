5dbc751be22de812eecbf387609e842f0059b346
Andreas Grobler <andreas@urbian.co.za>
2019-11-18 10:37:05 +0200

    Initial commit

---
+++ LifeCheqApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LifecheqappConfig(AppConfig):
+    name = 'LifeCheqApp'
---
+++ LifeCheqUser/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LifechequserConfig(AppConfig):
+    name = 'LifeCheqUser'
