c79ef101c9561744632a6d59e876e6374f74ac6a
Andreas Grobler <andreas@urbian.co.za>
2019-11-26 16:03:43 +0200

    Trying to fix static files on deployment

--- LifeCheq/settings.py
+++ LifeCheq/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,14 +132,14 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
-import django_heroku 
+
 
 django_heroku.settings(locals())
